d2dce4fa3e20e4ed405f8cc129c413f480b86555
Sebastian Ortega <sortega@tid.es>
2014-04-08 18:02:39 +0200

    Introduce backstage pass class

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -14,7 +14,7 @@ public class GildedRose {
         items.add(new AgedBrie(2, 0));
         items.add(new NormalItem("Elixir of the Mongoose", 5, 7));
         items.add(new Sulfuras());
-        items.add(new NormalItem("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        items.add(new BackstagePass(15, 20));
         items.add(new NormalItem("Conjured Mana Cake", 3, 6));
 
         updateQuality();
